80eae4d7f051331de967f8e58650d7f7fa7d9f9c
Diego Malagón <diegomalagonh@gmail.com>
2017-04-06 11:20:10 -0500

    Renombramiento de tabs

--- src/pages/pessimist/pessimist.ts
+++ src/pages/pessimist/pessimist.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: 'page-pessimist',
+  templateUrl: 'pessimist.html'
 })
-export class HomePage {
+export class PessimistPage {
 
   constructor(public navCtrl: NavController) {
 
--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,17 +1,17 @@
 import { Component } from '@angular/core';
 
-import { AboutPage } from '../about/about';
-import { ContactPage } from '../contact/contact';
-import { HomePage } from '../home/home';
+import { OptimistPage } from '../optimist/optimist';
+import { PessimistPage } from '../pessimist/pessimist';
+import { InfoPage } from '../info/info';
 
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
 
-  tab1Root = HomePage;
-  tab2Root = AboutPage;
-  tab3Root = ContactPage;
+  tab1Root = OptimistPage;
+  tab2Root = PessimistPage;
+  tab3Root = InfoPage;
 
   constructor() {
 
