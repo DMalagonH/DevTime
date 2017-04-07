5acacc11be2eee61f962e0cdabef79d5cf79551b
Diego Malagón <diegomalagonh@gmail.com>
2017-04-07 11:34:42 -0500

    Calculo de tiempo estimado optimista y pesimista

--- src/pages/optimist/optimist.ts
+++ src/pages/optimist/optimist.ts
@@ -1,19 +1,48 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
 
 @Component({
 	selector: 'page-optimist',
 	templateUrl: 'optimist.html'
 })
 export class OptimistPage {
+	times = {
+		min:	0,
+		max:	0,
+		pro:	0,
+		est:	0
+	};		
 
-	constructor(public navCtrl: NavController) {
+	constructor(public navCtrl: NavController, public alertCtrl: AlertController) {}
 
+	calculateOptimist = ():void => {
+		var estimated:number = (+this.times.min + +this.times.max + ( 4 * +this.times.pro)) / 6;
+
+		let alert = this.alertCtrl.create({
+			title:		"Tiempo estimado optimista",
+			subTitle:	estimated.toString(),
+			buttons: 	[ "Aceptar" ]
+		});
+
+		alert.present();
 	}
 
-	data = {
-		"tmin":	"",
-		"tmax":	"",
-		"tl":	"" 
-	};
+	calculatePessimist = ():void => {
+		var estimated:number = (+this.times.min + (3 * +this.times.max) + ( 2 * +this.times.pro)) / 6;
+		
+		let alert = this.alertCtrl.create({
+			title:		"Tiempo estimado pesimista",
+			subTitle:	estimated.toString(),
+			buttons: 	[ "Aceptar" ]
+		});
+
+		alert.present();
+	}
 }
+
+interface Times{
+	min:number,
+	max:number,
+	pro:number,
+	est:number
+};
